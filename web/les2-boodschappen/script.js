49fff99a165fa5f03726f58f30baebd9bd29aa5a
stenkristel <stenkristel2016@gmail.com>
2021-11-30 13:03:42 +0100

    Video games list gemaakt

--- web/les2-boodschappen/script.js
+++ web/les2-boodschappen/script.js
@@ -1,1 +1,22 @@
 console.log("scrips is geladen")
+
+const shootervak = document.getElementById('shooters');
+const platformervak = document.getElementById('platformers');
+
+function zetInShooters(shooters){
+    console.log(shooters);
+
+    const nieuwElement = document.createElement('h2');
+    nieuwElement.innerHTML = shooters;
+    nieuwElement.className = 'rood';
+    shootervak.appendChild(nieuwElement);
+}
+
+function zetInPlatformers(platformers){
+    console.log(platformers);
+
+    const nieuwElement = document.createElement('h2');
+    nieuwElement.innerHTML = platformers;
+    nieuwElement.className = 'groen';
+    platformervak.appendChild(nieuwElement);
+}
